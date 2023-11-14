917e09d924d0436e4f363d99fa7e812099d6eca7
Y <66672463+Yvancg@users.noreply.github.com>
2023-11-14 16:04:28 +0000

    fine tuning SQL

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///fitness.db")
+# Configure the use SQLite database
+db = SQL.connect("sqlite:///fitness.db")
 
 # Custom error handler
 @app.errorhandler(429)
