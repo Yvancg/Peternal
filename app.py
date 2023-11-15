c71458a88dcb29d98ebeda64e780160ce9521286
Y <66672463+Yvancg@users.noreply.github.com>
2023-11-15 13:46:33 +0000

    updates

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 limiter = Limiter(
     app=app,
     key_func=get_remote_address,
-    storage_uri="redis://localhost:6379"
+    storage_uri="redis://localhost:6379",
     default_limits=["5 per minute"]
 )
 
