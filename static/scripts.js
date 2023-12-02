8b754154a5f18b0b96400dedd75cfa3ceb2a35ad
Yvancg <yvancg@gmail.com>
2023-12-02 18:23:08 +0700

    add progress bar for password strength

--- static/scripts.js
+++ static/scripts.js
@@ -4,3 +4,35 @@ $(document).ready(function() {
         $(".alert").fadeOut('slow');
     }, 5000); // 5 seconds
 });
+
+// Tooltip function from Bootstrap 
+$(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip({
+        trigger: 'focus'
+    });
+});
+
+// Password strength progress bar
+document.getElementById('password').addEventListener('input', function() {
+    var password = this.value;
+    var strength = 0;
+
+    if (password.length > 7) strength += 20;
+    if (password.match(/[a-z]+/)) strength += 20;
+    if (password.match(/[A-Z]+/)) strength += 20;
+    if (password.match(/[0-9]+/)) strength += 20;
+    if (password.match(/[\W]+/)) strength += 20;
+
+    var strengthBar = document.getElementById('password-strength-bar');
+    strengthBar.style.width = strength + '%';
+
+    if (strength < 40) {
+        strengthBar.className = 'progress-bar bg-danger';
+    } else if (strength < 60) {
+        strengthBar.className = 'progress-bar bg-warning';
+    } else if (strength < 80) {
+        strengthBar.className = 'progress-bar bg-info';
+    } else {
+        strengthBar.className = 'progress-bar bg-success';
+    }
+});
