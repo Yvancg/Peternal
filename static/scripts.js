1564d2fd975b598c7c1f3736a4105dd2f7d29191
Yvancg <yvancg@gmail.com>
2023-12-19 00:56:11 +0700

    minor updates

--- static/scripts.js
+++ static/scripts.js
@@ -79,3 +79,25 @@ $(document).ready(function() {
         allowClear: true,
     });
 });
+
+// Replace photo and tracker info in pet cards
+function editPhoto(petId) {
+    // Logic to handle photo editing
+}
+
+function editTracker(petId) {
+    // Logic to handle tracker editing
+}
+
+// Login toggle
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
